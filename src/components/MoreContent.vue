b6e464a006cfaa1be2a620f3466aadf70f2d8e8a
unknown <ui_beam@mikutechnology.com>
2024-06-19 05:52:08 +0800

    first commit

---
+++ src/components/MoreContent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="more-content">一个什么都不会的小白，二次元浓度也不高，随便搞个小破站玩玩</div>
+</template>
+
+<style lang="scss" scoped>
+.more-content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+  width: 100%;
+  height: 100%;
+}
+</style>
